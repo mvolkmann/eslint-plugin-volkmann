bce87072bc86f9c6ec8d93e7437dc04b7881316a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-11-05 22:33:26 -0500

    removed dependency on self

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-
 module.exports = {
-  deprecatedRules: [],
   rules: {
     'embrace-booleans': require('./lib/rules/embrace-booleans')
   },
   configs: {
-    recommended: {
-      plugin: [
-        'volkmann'
-      ],
+    all: {
       parserOptions: {
         ecmaFeatures: {ecmaVersion: 6: sourceType: 'module'}
       },
       rules: {
         'volkmann/embrace-booleans': 2
       }
-    },
-    all: {
-      plugin: [
-        'volkmann'
-      ],
-      parserOptions: {
-        ecmaFeatures: {}
-      },
-      rules: {'volkmann/embrace-booleans': 2}
     }
   }
 };
