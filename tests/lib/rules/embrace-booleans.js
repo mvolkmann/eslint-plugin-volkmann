f3c437cbb2e370c79aca4c8a463e6c5560f16002
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-03-19 06:34:58 -0500

    got new tests from Kent C. Dodds to run

--- tests/lib/rules/embrace-booleans.js
+++ tests/lib/rules/embrace-booleans.js
@@ -1,52 +1,44 @@
-const {stripIndent} = require('common-tags')
-const {RuleTester} = require('eslint')
+const {RuleTester} = require('eslint');
 const ruleName = 'embrace-booleans';
 const rule = require('../../../lib/rules/' + ruleName);
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester();
 ruleTester.run(ruleName, rule, {
   valid: [
-    valid(`var x = y ? a : z`),
-    valid(`var x = y ? true : z`),
-    valid(`var x = y ? false : z`),
-    valid(`var x = y ? z : true`),
-    valid(`var x = y ? z : false`),
+    {code: 'var x = y ? a : z'},
+    {code: 'var x = y ? true : z'},
+    {code: 'var x = y ? false : z'},
+    {code: 'var x = y ? z : true'},
+    {code: 'var x = y ? z : false'}
   ],
   invalid: [
     invalid({
-      code: `var x = y ? true : false`,
-      output: `var x = Boolean(y)`,
-      usage: 'Boolean(y)',
+      code: 'var x = y ? true : false',
+      output: 'var x = Boolean(y)',
+      usage: 'Boolean(y)'
     }),
     invalid({
-      code: `var x = y ? false : true`,
-      output: `var x = !Boolean(y)`,
-      usage: '!Boolean(y)',
+      code: 'var x = y ? false : true',
+      output: 'var x = !Boolean(y)',
+      usage: '!Boolean(y)'
     }),
     invalid({
-      code: `var x = (y && z) ? false : true`,
-      output: `var x = !Boolean(y && z)`,
-      usage: '!Boolean(y && z)',
-    }),
+      code: 'var x = (y && z) ? false : true',
+      output: 'var x = !Boolean(y && z)',
+      usage: '!Boolean(y && z)'
+    })
   ],
-})
+});
 
-function valid(code) {
+function invalid({code, output, usage}) {
   return {
-    code: stripIndent([code]),
-  }
-}
-
-function invalid({code, output, usage, ...rest}) {
-  return {
-    code: stripIndent([code]),
-    output: stripIndent([output]),
+    code,
+    output,
     errors: [
       {
         message: `Unnecessary ternary; Use ${usage} instead`,
         type: 'ConditionalExpression',
       },
-    ],
-    ...rest,
-  }
+    ]
+  };
 }
