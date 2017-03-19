025d43faa075ba82005aa6bf2511b6a3449e548e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-03-19 07:24:17 -0500

    more work on ternary operators in JSX

--- tests/lib/rules/ternary-operator.js
+++ tests/lib/rules/ternary-operator.js
@@ -8,31 +8,59 @@ const rule = require('../../../lib/rules/' + ruleName);
 
 const ruleTester = new RuleTester();
 
-const badCode = `const result = x > 2
+const parserOptions = {
+  ecmaFeatures: {jsx: true},
+  ecmaVersion: 7,
+  sourceType: 'module'
+};
+
+const case1 = `const result = x > 2
   ? 'foo'
   : y > 2
       ? 'bar'
       : 'baz';
 `;
 
-const goodCode = `const result =
+const case1Expected = `const result =
   x > 2 ? 'foo' :
   y > 2 ? 'bar' :
   'baz';
+`;
+
+const case2 = `
+  <div>
+  {
+    hash === 'page1'
+      ? <Page1 />
+      : hash === 'page2'
+          ? <Page2 />
+          : <Home />
+  }
+  </div>
+`;
+
+const case2Expected = `
+  <div>
+  {
+    hash === 'page1' ? <Page1 /> :
+    hash === 'page2' ? <Page2 /> :
+    <Home />
+  }
+  </div>
 `;
 
 ruleTester.run(ruleName, rule, {
   //TODO: Currently every use of ternaries is treated like an error
   //TODO: and reformatted even if it is already formatted correctly.
   valid: [
-    {code: 'const a = b ? c : d;', parserOptions: {ecmaVersion: 7}},
-    {code: 'const a = b ? (c ? d : e) : f;', parserOptions: {ecmaVersion: 7}},
-    {code: goodCode, parserOptions: {ecmaVersion: 7}},
+    {code: 'const a = b ? c : d;', parserOptions},
+    {code: 'const a = b ? (c ? d : e) : f;', parserOptions},
+    {code: case1Expected, parserOptions},
   ],
   invalid: [
     {
       code: 'const a = b\n  ? c\n  : d;',
-      parserOptions: {ecmaVersion: 7},
+      parserOptions,
       errors: [
         {
           message: 'bad ternary formatting',
@@ -47,7 +75,7 @@ ruleTester.run(ruleName, rule, {
                : d
                    ? e
                    : f;`,
-      parserOptions: {ecmaVersion: 7},
+      parserOptions,
       errors: [
         {
           message: 'bad ternary formatting',
@@ -57,15 +85,28 @@ ruleTester.run(ruleName, rule, {
       output: 'const a =\n  b ? c :\n  d ? e :\n  f;'
     },
     {
-      code: badCode,
-      parserOptions: {ecmaVersion: 7},
+      code: case1,
+      parserOptions,
       errors: [
         {
           message: 'bad ternary formatting',
           type: 'VariableDeclaration'
         }
       ],
-      output: goodCode
+      output: case1Expected
+    },
+    /*
+    {
+      code: case2,
+      parserOptions,
+      errors: [
+        {
+          message: 'bad ternary formatting',
+          type: 'VariableDeclaration'
+        }
+      ],
+      output: case2Expected
     }
+    */
   ]
 });
